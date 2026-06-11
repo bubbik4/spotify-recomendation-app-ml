17e0b39dcb76450654c28e1b97b104107b4e1a85
bubbik4 <piotrlubik047@duck.com>
2026-06-11 19:15:58 +0200

    feat(ui): implement Streamlit view with search input and ML logic integration

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,23 +1,41 @@
 import streamlit as st
 import pandas as pd
 import os
+from recommender import load_models, get_recommendation
 
 # Konfiguracja głównego okna aplikacji
 st.set_page_config(page_title="Music Recommender", page_icon="🎵", layout="wide")
 
+# Model ML to cache, so it would olny load on server startup
+@st.cache_resource
+def get_ml_components():
+    return load_models()
+
+# CSV to RAM
+@st.cache_data
+def load_data():
+    current_dir = os.path.dirname(__file__)
+    data_path = os.path.join(current_dir, '..', 'data', 'spotify_cleaned.csv')
+    return pd.read_csv(data_path)
+    
+# backend init
+model, scaler = get_ml_components()
+df = load_data()
+features = ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+
+# main i-face
 st.title("🎵 System Rekomendacji Muzyki")
-st.write("Witaj! Jeśli widzisz ten tekst, Twój lokalny serwer Streamlit działa poprawnie.")
+st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
 
-# Bezpieczne budowanie ścieżki niezależnie od systemu operacyjnego
-# Zakładamy, że jesteśmy w 'app/streamlit_app.py', więc musimy cofnąć się o poziom wyżej
-current_dir = os.path.dirname(__file__)
-data_path = os.path.join(current_dir, '..', 'data', 'spotify_cleaned.csv')
+search_query = st.text_input("Szukaj utworu:", placeholder="np. Numb")
 
-# Test podłączenia warstwy danych
-try:
-    df = pd.read_csv(data_path)
-    st.success(f"Warstwa analityczna podpięta pomyślnie! Liczba utworów w bazie: {len(df)}")
-    # Interaktywna tabela na frontendzie (będziesz mógł ją sortować i przeszukiwać w przeglądarce)
-    st.dataframe(df.head(5))
-except FileNotFoundError:
-    st.error(f"Nie znaleziono pliku z danymi. Oczekiwana ścieżka: {data_path}")
+# search logic
+if search_query:
+    with st.spiner("Przeszukuję bazę i analizuję wektory..."):
+        results = get_recommendation(search_query, df, model, scaler, features)
+
+        if results is not None:
+            st.success("Znaleziono podobne utwory!")
+            st.dataframe(results)
+        else:
+            st.error("Nie znaleziono takiego utworu. Spróbuj wpisać inny tytuł")
